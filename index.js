a873750feea105931baa4610335866765a65befc
sharmakshay <sharmakshay@icloud.com>
2017-07-24 04:55:57 -0400

    added functionality to get all sources for a specific language, country, and category

--- index.js
+++ index.js
@@ -40,11 +40,12 @@ var loadData = function(){
     lang = lang.substring(0,2);
     if (!languaguekeys.includes(lang)){
         lang = "en";
+        console.log("language not found, set to default language");
     }
     
     // get user location
     if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(showPos);
+        navigator.geolocation.getCurrentPosition(getPosition);
     }
     else{
         console.log("Geolocation is not supported by this browser");
@@ -53,23 +54,31 @@ var loadData = function(){
         console.log(country);
     }
 
-    category = $('#category');
+    categoryDOM = $('#category');
     $.each(categorykeys, function(){
-        category.append($("<option />").val(this).text(listCategories[this]));
+        categoryDOM.append($("<option />").val(this).text(listCategories[this]));
     });
-    category.addClass("ui search dropdown optionsstyle");
+    categoryDOM.addClass("ui search dropdown optionsstyle");
     
-    $('.ui.dropdown').dropdown();
+    $('#category').dropdown({
+        onChange: function () {
+            category = categoryDOM.val();
+            console.log("getting news for " + lang + country + category);
+            getSources(lang, country, category)
+        }
+    });
 }
 
+//**************** Functions used in loadData() ****************//
+
 // get lat, long of user
-var showPos = function(position){
+var getPosition = function(position){
     lat = position.coords.latitude;
     long = position.coords.longitude;
     getCountry(lat, long);
 }
 
-// convert location to country
+// convert lat, long to country
 var getCountry = function(la, lo){
     requrl = "http://ws.geonames.org/countryCodeJSON?lat=" + la + "&lng=" + lo + "&username=veracity";
     var xhttp = new XMLHttpRequest();
@@ -77,5 +86,21 @@ var getCountry = function(la, lo){
     //xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
     var response = JSON.parse(xhttp.responseText);
+    country = response['countryCode'].toLowerCase();
+    console.log(country);
+}
+
+// get news sources for a specific language, country, and category
+var getSources = function (la, cty, cat) {
+    requrl = "https://newsapi.org/v1/sources?language=" + la + "&country=" + cty + "&category=" + cat;
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", requrl, false);
+    xhttp.send();
+    var response = JSON.parse(xhttp.responseText);
     console.log(response);
+    var sources = response['sources']
+    var sourceIDs = []
+    for (var source in sources){
+        sourceIDs.push(source['id']);
+    }
 }
